2a630f8b6bbca3dfb91a5ed4cf6915b4a7001550
sq1533 <618356@naver.com>
2025-01-07 19:01:17 +0900

    feat : class 메서드 가져오기

--- home.py
+++ home.py
@@ -1,32 +1,40 @@
 from datetime import datetime
 
-example ={
-    "name":"서동규",
-    "join":"2021-09-30",
-    "usedPTO":"24"
+example = {
+    100110 : 
+    {
+        "name":"서동규",
+        "join":"2021-09-30",
+        "usedPTO":"1"
     }
+}
 
 #연차 계산기
 class aboutPTO:
     #데이터 정의
     def __init__(self):
         self.worker = example
-    #입사 후 토탈 연차
-    def PTO(self):
-        join = self.worker["join"].split("-")
-        year = int(join[0])
-        month = int(join[1])
-        day = int(join[2])
-        workYear = int(datetime.now().strftime('%Y')) - year
-        if workYear == 0:
-            pto = int(datetime.now().strftime('%m')) - month
-        else:
-            pto = (workYear-1)*15 + (12 - month)
-        return pto
-    #남은 연차
-    def leftPTO(self):
-        left = self.PTO() - int(example["usedPTO"])
-        return left
+user = aboutPTO()
+#입사 후 토탈 연차
+def PTO(worker:aboutPTO):
+    join = worker.__init__["join"].split("-")
+    year = int(join[0])
+    month = int(join[1])
+    day = int(join[2])
+    workYear = int(datetime.now().strftime('%Y')) - year
+    if workYear == 0:
+        pto = int(datetime.now().strftime('%m')) - month
+    else:
+        pto = (workYear-1)*15 + (12 - month)
+    return pto
 
-PTO = aboutPTO()
-print(PTO.leftPTO())
+#사용 연차
+def usedPTO(worker:aboutPTO):
+    return worker["usedPTO"]
+
+#남은 연차
+def leftPTO(pto:PTO):
+    left = pto
+    return left
+
+print(PTO(100110))
