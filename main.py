c1f81ec770d3527b0fa01354a4a67665e4c69161
sq1533 <618356@naver.com>
2025-01-10 09:57:56 +0900

    feat : post 요청 htmx처리

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 import pandas as pd  # Assuming you need pandas for data processing
 import re  # Assuming you need re for data processing
 from fastapi import FastAPI, Request, Form
@@ -11,20 +12,13 @@ templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "t
 
 # MainPage
 @app.get("/home", response_class=HTMLResponse)
-async def HOME(request: Request):
+async def HOME(request:Request):
     return templates.TemplateResponse(name="HCM.html",context={"request":request})
 
-@app.get("/{LOOKUP}")
-async def read_item(request: Request,LOOKUP:str):
-    test = LOOKUP+"123"
-    return {"LOOKUP":test}
-
-class User(BaseModel):
-    username: str
-
-@app.post("/submit")
-async def create_user(request: Request, user: User = Form(...)):
-    return {"message": f"Hello, {user.username}!"}
+@app.post("/lookup")
+async def read_number(request: Request):
+    data = await request.form()
+    return {"message":"Hello,"+data["number"]}
 
 if __name__ == "__main__":
     import uvicorn
