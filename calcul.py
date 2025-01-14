9ba02aac09f061ee1e62812ec171b64a130979eb
sq1533 <618356@naver.com>
2025-01-14 17:34:44 +0900

    feat : html 분리, 공휴일 근무/연차 취소 로직 추가

--- calcul.py
+++ calcul.py
@@ -1,57 +1,75 @@
+import os
+import json
 from datetime import datetime
 
-example = {
-    "21093001" : 
-    {
-        "name":"서동규",
-        "join":"2021-09-30",
-        "usedPTO":"10",
-        "addPTO":"4"
-    }
-}
+#DB데이터 경로
+staffInfoPath = os.path.join(os.path.dirname(__file__),"DB","staffInfo.json")
 
 #연차 계산기
 class aboutPTO:
     #데이터 정의
     def __init__(self,user):
-        self.name = example[user]["name"]
-        self.join = example[user]["join"]
-        self.usedPTO = example[user]["usedPTO"]
-        self.add = example[user]["addPTO"]
+        with open(staffInfoPath, 'r', encoding='utf-8') as f:
+            info = json.load(f)
+        self.user = user
+        self.join = info[user]["join"]
+        self.usedPTO = info[user]["usedPTO"]
+        self.add = info[user]["addPTO"]
 
-#입사 후 전체 연차
+#입사 후 전체 연차(get)
 def totalPTO(info:aboutPTO) -> str:
     today = datetime.now()
+    todayY = float(today.strftime('%Y'))
+    todayM = float(today.strftime('%m'))
     join = info.join.split("-")
-    year = int(join[0])
-    month = int(join[1])
-    workYear = int(today.strftime('%Y')) - year
+    year = float(join[0])
+    month = float(join[1])
+    workYear = todayY - year
     if workYear == 0:
-        pto = int(today.strftime('%m')) - month + int(info.add)
+        pto = todayM - month
+    elif workYear == 1:
+        if todayM < month:
+            pto = 12 + todayM - month
+        else:
+            pto = (workYear-1)*15 + (12 - month)
     else:
-        pto = (workYear-1)*15 + (12 - month) + int(info.add)
+        pto = (workYear-1)*15 + (12 - month)
     return str(pto)
 
-#사용 연차
-def usedPTO(worker:aboutPTO) -> str:
-    return worker.usedPTO
-
-#남은 연차
-def leftPTO(tPTO:totalPTO,uPTO:usedPTO) -> str:
-    left = int(tPTO) - int(uPTO)
+#남은 연차(get)
+def leftPTO(tPTO:str,exPTO:str,uPTO:str) -> str:
+    left = float(tPTO) + float(exPTO) - float(uPTO)
     return str(left)
 
-#사용 연차 누계
+#사용 연차 누계(post)
 def addUsedPTO(worker:aboutPTO,data:str) -> str:
-    used = int(worker.usedPTO)
-    if data == "annual":
-        result = used + 1
-        return str(result)
-    elif data == "half":
-        result = used + 0.5
-        return str(result)
-    elif data == "half_half":
-        result = used + 0.25
-        return str(result)
-    else:
-        return str(used)
+    used = float(worker.usedPTO)
+    result = used + float(data)
+    with open(staffInfoPath, 'r', encoding='utf-8') as j:
+        workerinfo = json.load(j)
+    workerinfo[worker.user]["usedPTO"] = str(result)
+    with open(staffInfoPath, 'w', encoding='utf-8') as j:
+        json.dump(workerinfo, j, ensure_ascii=False, indent=4)
+    return str(result)
+
+#추가 연차(post)
+def extraPTO(worker:aboutPTO,number:str) -> str:
+    extra = float(worker.add)
+    result = extra + (float(number)*1.5)
+    with open(staffInfoPath, 'r', encoding='utf-8') as j:
+        workerinfo = json.load(j)
+    workerinfo[worker.user]["addPTO"] = str(result)
+    with open(staffInfoPath, 'w', encoding='utf-8') as j:
+        json.dump(workerinfo, j, ensure_ascii=False, indent=4)
+    return str(result)
+
+#연차 사용 취소(post)
+def cancelPTO(worker:aboutPTO,number:str) -> str:
+    usedPTO = float(worker.usedPTO)
+    result = usedPTO - float(number)
+    with open(staffInfoPath, 'r', encoding='utf-8') as j:
+        workerinfo = json.load(j)
+    workerinfo[worker.user]["usedPTO"] = str(result)
+    with open(staffInfoPath, 'w', encoding='utf-8') as j:
+        json.dump(workerinfo, j, ensure_ascii=False, indent=4)
+    return str(result)
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import os
-import pandas as pd  # Assuming you need pandas for data processing
-import re  # Assuming you need re for data processing
-from fastapi import FastAPI, Request, Form
+import json
+from fastapi import FastAPI, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
 
 import calcul
 
 app = FastAPI()
 templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "templates"))
+
+#근무자 정보 path
+staffInfoPath = os.path.join(os.path.dirname(__file__),"DB","staffInfo.json")
 
 # MainPage
 @app.get("/home",response_class=HTMLResponse)
@@ -20,15 +21,58 @@ async def HOME(request:Request):
 @app.post("/lookup")
 async def read_number(request:Request):
     data = await request.form()
-    return data
+    with open(staffInfoPath, 'r', encoding='utf-8') as j:
+        workerinfo = json.load(j)
+    if data["name"] not in list(workerinfo.keys()):
+        return Response(content="근무자 이름이 잘못되었습니다.")
+    else:
+        user = calcul.aboutPTO(data["name"])
+        userName = calcul.aboutPTO(data["name"]).user
+        userUsedPTO = calcul.aboutPTO(data["name"]).usedPTO
+        userExtraPTO = calcul.aboutPTO(data["name"]).add
+        userTotalPTO = calcul.totalPTO(info=user)
+        userleftPTO = calcul.leftPTO(tPTO=userTotalPTO,exPTO=userExtraPTO,uPTO=userUsedPTO)
+        results = f"[근무자 : {userName}] [남은연차 : {userleftPTO}]"
+        return templates.TemplateResponse(request=request,name="output.html",context={"results":results,"name":userName})
 
 #연차 사용 처리
 @app.post("/usingPTO")
 async def using(request:Request):
     data = await request.form()
-    user = calcul.aboutPTO(data["number"])
-    results = calcul.addUsedPTO(worker=user,data=data["time"])
-    return results
+    with open(staffInfoPath, 'r', encoding='utf-8') as j:
+        workerinfo = json.load(j)
+    if data["name"] not in list(workerinfo.keys()):
+        return Response(content="근무자 이름이 잘못되었습니다.")
+    else:
+        user = calcul.aboutPTO(data["name"])
+        results = calcul.addUsedPTO(worker=user,data=data["time"])
+        return Response(content=results)
+
+#추가 연차 반영
+@app.post("/addPTO")
+async def add(request:Request):
+    data = await request.form()
+    with open(staffInfoPath, 'r', encoding='utf-8') as j:
+        workerinfo = json.load(j)
+    if data["name"] not in list(workerinfo.keys()):
+        return Response(content="근무자 이름이 잘못되었습니다.")
+    else:
+        user = calcul.aboutPTO(data["name"])
+        results = calcul.extraPTO(worker=user,number=data["add"])
+        return Response(content=results)
+
+#연차 사용 취소
+@app.post("/cancelPTO")
+async def cancel(request:Request):
+    data = await request.form()
+    with open(staffInfoPath, 'r', encoding='utf-8') as j:
+        workerinfo = json.load(j)
+    if data["name"] not in list(workerinfo.keys()):
+        return Response(content="근무자 이름이 잘못되었습니다.")
+    else:
+        user = calcul.aboutPTO(data["name"])
+        results = calcul.cancelPTO(worker=user,number=data["cancel"])
+        return Response(content=results)
 
 if __name__ == "__main__":
     import uvicorn
